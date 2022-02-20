a2b8c5c6506ae27de92bc872f74f0987ef080640
Daniel <dlemmonsqueeze@gmail.com>
2022-02-20 08:30:28 -0700

    rearranged text, and layout

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ track1_icon.onclick = function () {
   if (track1.paused) {
     track1.play();
     track1_icon.src = "/public/images/pause.png";
+    document.getElementById("eq").style.display = "inline-block";
   } else {
     track1.pause();
     track1_icon.src = "/public/images/play.png"
+    document.getElementById("eq").style.display = "none";
   }
 }
