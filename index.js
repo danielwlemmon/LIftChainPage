aeffb15a61f93763d94e979530604d2e915190ba
Daniel <dlemmonsqueeze@gmail.com>
2022-02-20 09:42:10 -0700

    try touchstart

--- index.js
+++ index.js
@@ -16,5 +16,16 @@ window.onload = function () {
     };
   });
 
+  track1_icon.addEventListener("touchstart", function () {
+    if (track1.paused) {
+      track1.play();
+      track1_icon.src = "public/images/pause.png";
+      document.getElementById("eq").style.display = "inline-block";
+    } else {
+      track1.pause();
+      track1_icon.src = "public/images/Play.png"; //force update
+      document.getElementById("eq").style.display = "none";
+    };
+  });
   
 };
