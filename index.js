5bbd3af5ea1cfef08d8449a444d50bc8e048ffd1
Daniel <dlemmonsqueeze@gmail.com>
2022-02-20 09:14:23 -0700

    fixing the javascript error

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const track1 = document.getElementById("1");
 const track1_icon = document.getElementById("icon1");
 
-track1_icon.onclick = function () {
-  if (track1.paused) {
-    track1.play();
-    track1_icon.src = "/public/images/pause.png";
-    document.getElementById("eq").style.display = "inline-block";
-  } else {
-    track1.pause();
-    track1_icon.src = "/public/images/play.png"
-    document.getElementById("eq").style.display = "none";
-  }
-}
+window.onload = function () {
+  track1_icon.onclick = function () {
+    if (track1.paused) {
+      track1.play();
+      track1_icon.src = "/public/images/pause.png";
+      document.getElementById("eq").style.display = "inline-block";
+    } else {
+      track1.pause();
+      track1_icon.src = "/public/images/play.png";
+      document.getElementById("eq").style.display = "none";
+    };
+  };
+};
