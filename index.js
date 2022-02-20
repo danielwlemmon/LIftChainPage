25250cee267ad8853318d77a63eca22f89f855a8
Daniel <dlemmonsqueeze@gmail.com>
2022-02-20 10:39:07 -0700

    updated bio

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ window.onload = function () {
   const track1 = document.getElementById("1");
   const track1_icon = document.getElementById("icon1");
 
-
   track1_icon.addEventListener("click", function () {
     if (track1.paused) {
       track1.play();
@@ -11,21 +10,9 @@ window.onload = function () {
       document.getElementById("eq").style.display = "inline-block";
     } else {
       track1.pause();
-      track1_icon.src = "public/images/Play.png"; //force update
+      track1_icon.src = "public/images/Play.png";
       document.getElementById("eq").style.display = "none";
     };
   });
 
-  track1_icon.addEventListener("touchstart", function () {
-    if (track1.paused) {
-      track1.play();
-      track1_icon.src = "public/images/pause.png";
-      document.getElementById("eq").style.display = "inline-block";
-    } else {
-      track1.pause();
-      track1_icon.src = "public/images/Play.png"; //force update
-      document.getElementById("eq").style.display = "none";
-    };
-  });
-  
 };
