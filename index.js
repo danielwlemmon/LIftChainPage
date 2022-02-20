0d41576603226b0c358d3d376e73485c3fcde5c2
Daniel <dlemmonsqueeze@gmail.com>
2022-02-20 09:22:32 -0700

    more route updates

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ window.onload = function () {
     console.log("click registered");
     if (track1.paused) {
       track1.play();
-      track1_icon.src = "/public/images/pause.png";
+      track1_icon.src = "public/images/pause.png";
       document.getElementById("eq").style.display = "inline-block";
     } else {
       track1.pause();
-      track1_icon.src = "/public/images/play.png";
+      track1_icon.src = "public/images/play.png";
       document.getElementById("eq").style.display = "none";
     };
   };
